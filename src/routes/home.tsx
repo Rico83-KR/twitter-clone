4bc7bba821ebe1658a8dec062479b7c1f7d2b903
Dinguri-PC\Dinguri <lllbthere00@gmail.com>
2023-12-08 22:15:31 +0900

    Firestore & Send Twit

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,17 +1,12 @@
-import { useNavigate } from "react-router-dom";
-import { auth } from "../firebase";
-import { Button, Title, Wrapper } from "../components/auth-components";
+import { styled } from "styled-components";
+import PostTweetForm from "../components/post-tweet-form";
+
+const Wrapper = styled.div``;
 
 export default function Home() {
-  const navigate = useNavigate();
-  const logOut = async () => {
-    await auth.signOut();
-    navigate("/login");
-  };
   return (
     <Wrapper>
-    <Title>Here is to be Eden</Title>
-    <Button onClick={logOut}>Log Out</Button>
+      <PostTweetForm />
     </Wrapper>
   );
 }
